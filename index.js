5b7f7245a9931961599d00237526fb10178dc9e7
Tracey Jaquith <tracey@archive.org>
2022-02-12 12:42:56 -0800

    wth

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 
+serve(() => new Response('hello from HinD'), {
+  port: 6666, // Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
+})
+
+/*
 serve(() => {
-  console.log(`visitor.  demo secrets: ${Deno.env.get('TOKEN3')} ${Deno.env.get('MY_URL')}`)
+  // eslint-disable-next-line no-console
+//  console.log(`visitor.  demo secrets: ${Deno.env.get('TOKEN3')} ${Deno.env.get('MY_URL')}`)
   return new Response('hello from HinD')
 }, {
   port: Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
 })
+*/
