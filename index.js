a98005da674ed59d4669b2404b77772462876e98
Tracey Jaquith <tracey@archive.org>
2022-02-12 12:17:57 -0800

    show how can use additional GH secrets.  better documentation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 
-serve(() => new Response('hello from HinD'), {
+serve(() => {
+  console.log(`visitor.  demo secrets: ${Deno.env.get('TOKEN3')} ${Deno.env.get('MY_URL')}`)
+  return new Response('hello from HinD')
+}, {
   port: Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
 })
