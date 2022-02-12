3f3e729522d045291e26d26957f71a890e6f7a18
Tracey Jaquith <tracey@archive.org>
2022-02-12 12:43:09 -0800

    comments, re-indents, etc (nothing substantitive)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 
 serve(() => new Response('hello from HinD'), {
-  port: 6666, // Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
+  port: Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
 })
 
 /*
