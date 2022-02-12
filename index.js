f7685f6f51314373199e0fa2a011d40abb887f32
Tracey Jaquith <tracey@archive.org>
2022-02-12 12:49:32 -0800

    lets try this again

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import { serve } from 'https://deno.land/std/http/server.ts'
 
-serve(() => new Response('hello from HinD'), {
-  port: Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
-})
+// eslint-disable-next-line no-console
+console.log(`visitor.  demo secrets: ${Deno.env.get('TOKEN3')} ${Deno.env.get('MY_URL')}`)
 
-/*
+/* serve(() => new Response('hello from HinD'), {
+  port: Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
+}) */
+
 serve(() => {
   // eslint-disable-next-line no-console
-//  console.log(`visitor.  demo secrets: ${Deno.env.get('TOKEN3')} ${Deno.env.get('MY_URL')}`)
+  console.log(`visitor.  demo secrets: ${Deno.env.get('TOKEN3')} ${Deno.env.get('MY_URL')}`)
   return new Response('hello from HinD')
 }, {
   port: Number(Deno.env.get('NOMAD_PORT_http')) || 5000,
 })
-*/
